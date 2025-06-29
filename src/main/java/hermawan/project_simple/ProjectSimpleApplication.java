6ced6b85b2ed8be033b03ce837a1b0587aaee9f9
Arief-Karditya <nolifegansaya@gmail.com>
2025-06-29 16:05:04 +0700

    Batman

---
+++ src/main/java/hermawan/project_simple/ProjectSimpleApplication.java
@@ -0,0 +1,13 @@
+package hermawan.project_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hermawan/project_simple/ProjectSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package hermawan.project_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
